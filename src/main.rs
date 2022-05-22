85bb7d83f95bc51160c347e397ef14f805a6b19f
Adrian Parvin Ouano <adrianparvino@gmail.com>
2022-05-22 09:26:54 +0800

    Decrease X11 communication and throttle to 1/ms.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,103 @@
+use std::os::unix::prelude::AsRawFd;
+use std::task::Poll;
+
 use x11rb::connection::Connection;
 use x11rb::rust_connection::RustConnection;
 use x11rb::protocol::xinput::XIEventMask;
 use x11rb::protocol::xinput::DeviceUse;
 use x11rb::protocol::Event;
 use x11rb::protocol::xtest::fake_input;
+use nix::poll::{poll, PollFd, PollFlags};
 use anyhow::Result;
 
-const SENSITIVITY: i32 = 4;
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum XDirection {
+    Left,
+    Right,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum YDirection {
+    Up,
+    Down
+}
+
+struct MouseHandle<'conn> {
+    x: Option<XDirection>,
+    y: Option<YDirection>,
+    conn: &'conn RustConnection,
+    root: u32
+}
+
+impl<'conn> MouseHandle<'conn> {
+    fn new(conn: &'conn RustConnection, root: u32) -> Self {
+        Self {
+            x: None,
+            y: None,
+            conn,
+            root
+        }
+    }
+
+    fn change_x(&mut self, new_x: Option<XDirection>) -> Result<()> {
+        if self.x == new_x { return Ok(()); }
+
+        match self.x {
+            None => {},
+            Some(XDirection::Left) => {
+                fake_input(self.conn, 3, 113, 0, self.root, 0, 0, 2)?.check()?;
+            }
+            Some(XDirection::Right) => {
+                fake_input(self.conn, 3, 114, 0, self.root, 0, 0, 2)?.check()?;
+            }
+        }
+
+        match new_x {
+            None => {},
+            Some(XDirection::Left) => {
+                fake_input(self.conn, 2, 113, 0, self.root, 0, 0, 2)?.check()?;
+            }
+            Some(XDirection::Right) => {
+                fake_input(self.conn, 2, 114, 0, self.root, 0, 0, 2)?.check()?;
+            }
+        }
+
+        self.x = new_x;
+
+        Ok(())
+    }
+
+
+    fn change_y(&mut self, new_y: Option<YDirection>) -> Result<()> {
+        if self.y == new_y { return Ok(()); }
+
+        match self.y {
+            None => {},
+            Some(YDirection::Up) => {
+                fake_input(self.conn, 3, 116, 0, self.root, 0, 0, 2)?.check()?;
+            }
+            Some(YDirection::Down) => {
+                fake_input(self.conn, 3, 111, 0, self.root, 0, 0, 2)?.check()?;
+            }
+        }
+
+        match new_y {
+            None => {},
+            Some(YDirection::Up) => {
+                fake_input(self.conn, 2, 116, 0, self.root, 0, 0, 2)?.check()?;
+            }
+            Some(YDirection::Down) => {
+                fake_input(self.conn, 2, 111, 0, self.root, 0, 0, 2)?.check()?;
+            }
+        }
+
+        self.y = new_y;
+
+        Ok(())
+    }
+}
+
+const SENSITIVITY: i32 = 10;
 
 fn main() -> Result<()> {
     let (conn, screen) = RustConnection::connect(None).unwrap();
@@ -36,10 +127,12 @@ fn main() -> Result<()> {
     };
     let mut delta = (0, 0);
 
-    let mut x = None;
-    let mut y = None;
+    let mut mouse = MouseHandle::new(&conn, root);
+
+    let mut fds = [ PollFd::new(conn.stream().as_raw_fd(), PollFlags::POLLIN) ];
 
     loop {
+        poll(&mut fds, 1);
         if let Ok(Some(event)) = conn.poll_for_event() {
             match event {
                 Event::XinputMotion(event) => {
@@ -60,47 +153,23 @@ fn main() -> Result<()> {
         }
 
         if delta.0 > SENSITIVITY {
-            x = Some(true);
-            fake_input(&conn, 2, 114, 0, root, 0, 0, 2)?.check()?;
-
+            mouse.change_x(Some(XDirection::Right))?;
             delta.0 -= SENSITIVITY;
         } else if delta.0 < -SENSITIVITY {
-            x = Some(false);
-            fake_input(&conn, 2, 113, 0, root, 0, 0, 2)?.check()?;
-
+            mouse.change_x(Some(XDirection::Left))?;
             delta.0 += SENSITIVITY;
         } else {
-            match x {
-                Some(true) => {
-                    fake_input(&conn, 3, 114, 0, root, 0, 0, 2)?.check()?;
-                },
-                Some(false) => {
-                    fake_input(&conn, 3, 113, 0, root, 0, 0, 2)?.check()?;
-                }
-                _ => ()
-            }
+            mouse.change_x(None)?;
         }
 
         if delta.1 > SENSITIVITY {
-            y = Some(true);
-            fake_input(&conn, 2, 116, 0, root, 0, 0, 2)?.check()?;
-
+            mouse.change_y(Some(YDirection::Up))?;
             delta.1 -= SENSITIVITY;
         } else if delta.1 < -SENSITIVITY {
-            y = Some(false);
-            fake_input(&conn, 2, 111, 0, root, 0, 0, 2)?.check()?;
-
+            mouse.change_y(Some(YDirection::Down))?;
             delta.1 += SENSITIVITY;
         } else {
-            match y {
-                Some(true) => {
-                    fake_input(&conn, 3, 116, 0, root, 0, 0, 2)?.check()?;
-                },
-                Some(false) => {
-                    fake_input(&conn, 3, 111, 0, root, 0, 0, 2)?.check()?;
-                }
-                _ => ()
-            }
+            mouse.change_y(None)?;
         }
     }
 }
